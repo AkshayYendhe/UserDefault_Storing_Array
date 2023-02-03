33197ab2e64a432a99e2b92825b614e4e8d9aa0f
forloop <yendheakshayd@gmail.com>
2023-02-03 11:12:32 +0530

    UserDefault_Storing_Array

--- UserDefault_Storing_Array/ViewController.swift
+++ UserDefault_Storing_Array/ViewController.swift
@@ -7,13 +7,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+   
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var ageTextField: UITextField!
+    
+    @IBOutlet weak var myStatusLabel: UILabel!
+    
+    var userArray = [String]()
+   
+    
+    @IBOutlet weak var myTable: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print(FileManager.default.temporaryDirectory)
+        
+        if let data = UserDefaults().object(forKey: nameTextField.text!) as? [String] {
+            
+            userArray = data
+            myStatusLabel.text = "\(userArray.count) user's found."
+            
+        }
+        else{
+            myStatusLabel.text = "User Not Found!"
+        }
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func saveButtonClicked(_ sender: Any) {
+        
+        if let email = nameTextField.text ,let pass = ageTextField.text, let password = Int(pass), (!email.isEmpty && !pass.isEmpty)  {
+            userArray = ["\(email)","\(password)"]
+            UserDefaults().set(userArray, forKey: "\(email)")
+            myTable.reloadData()
+            myStatusLabel.text = "New User Added!"
+            nameTextField.text = ""
+            ageTextField.text = ""
+        }
+        else{
+            myStatusLabel.text = "Kindly Enter Email & Password"
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return userArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = myTable.dequeueReusableCell(withIdentifier: "cell",for: indexPath)
+        cell.textLabel?.text = userArray[indexPath.row]
+        return cell
+    }
+    
 }
 
